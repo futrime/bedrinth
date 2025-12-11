750d5abffcba3e1facc6820dfa519b9ecf8c25a1
harryxi <harry-xi@outlook.com>
2025-12-11 23:12:19 +0800

    fix: move @cloudflare/next-on-pages to @opennextjs/cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
+let cloudflareConfig = {};
 if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
+  cloudflareConfig = defineCloudflareConfig()
 }
+
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["next-mdx-remote"],
 };
 
-export default nextConfig;
+export default {...cloudflareConfig,...nextConfig};
