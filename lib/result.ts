3f8871186b47a0d4c5d64fe47e1e75d9e52c023c
Zijian Zhang <35801754+futrime@users.noreply.github.com>
2026-02-19 10:38:32 +0800

    chore: remove everything

--- lib/result.ts
+++
@@ -1,8 +0,0 @@
-export type Result<T, E> =
-  | { ok: true; err: false; val: T }
-  | { ok: false; err: true; val: E };
-
-export const Result = {
-  Ok: <T>(val: T) => ({ ok: true as const, err: false as const, val: val }),
-  Err: <E>(val: E) => ({ ok: false as const, err: true as const, val: val }),
-};
