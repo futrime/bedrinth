367167bf0de68a48b72424cbd6598a3b5fa117e1
ShrBox <shrbox@outlook.com>
2026-03-15 22:46:30 +0800

    fix: fix logo on cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    loader: "custom",
+    loaderFile: "./image-loader.ts",
+  },
+};
 
 export default nextConfig;
